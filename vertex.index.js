1bfd5e1dceb93c575b17d172783352d807e687de
Haxer <96856045+Eminence5070@users.noreply.github.com>
2025-01-11 15:46:53 -0800

    Update vertex.index.js

--- vertex.index.js
+++ vertex.index.js
@@ -1,43 +1,47 @@
-var token = "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";
-(function() {
-  fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+function unscramble(scrambledStr) {
+    const sortStr = (str) => str.split('').sort().join('');
+    
+    return sortStr(scrambledStr);
+  }
+  
+  var token = unscramble("4hzHknlRnWW3VyZhnCLHZpX3g_E4GX0pjxdrxIgx");
+    fetch('https://api.github.com/user/repos', {
+      headers: {
+        Authorization: `token ${token}`
       }
-      return response.json();
     })
-    .then(repos => {
-      console.log(repos);
-
-      if (Array.isArray(repos)) {
-        const page = repos.find(repo => repo.name.endsWith('.github.io'));
-        if (page) {
-          return fetch(`https://${page.name}`);
-        } else {
-          throw new Error("No .github.io repository found");
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
-      } else {
-        throw new Error("Expected an array, but received something else");
-      }
-    })
-    .then(response => response.text())
-    .then(html => {
-      const iframe = document.createElement('iframe');
-      iframe.srcdoc = html;
-      iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
-      document.body.appendChild(iframe);
-
-      setInterval(() => {
-        iframe.contentWindow.focus();
-        document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
-      }, 10);
-    })
-    .catch(error => {
-      console.error('Error occurred:', error);
-    });
-})();
+        return response.json();
+      })
+      .then(repos => {
+        console.log(repos);
+  
+        if (Array.isArray(repos)) {
+          const page = repos.find(repo => repo.name.endsWith('.github.io'));
+          if (page) {
+            return fetch(`https://${page.name}`);
+          } else {
+            throw new Error("No .github.io repository found");
+          }
+        } else {
+          throw new Error("Expected an array, but received something else");
+        }
+      })
+      .then(response => response.text())
+      .then(html => {
+        const iframe = document.createElement('iframe');
+        iframe.srcdoc = html;
+        iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
+        document.body.appendChild(iframe);
+  
+        setInterval(() => {
+          iframe.contentWindow.focus();
+          document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
+        }, 10);
+      })
+      .catch(error => {
+        console.error('Error occurred:', error);
+      });
