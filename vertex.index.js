fa3a3bc160e22faaa5e053039e7332000193e6e9
Haxer <96856045+Eminence5070@users.noreply.github.com>
2025-01-11 15:31:08 -0800

    Update vertex.index.js

--- vertex.index.js
+++ vertex.index.js
@@ -1,23 +1,43 @@
-var token = "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"
+var token = "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";
+(function() {
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(repos => {
+      console.log(repos);
 
-
-fetch('//api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
-  .then(response => { return response.json(); })
-  .then(repos => { const page = repos.find(repo => repo.name.endsWith('.github.io')); return fetch(`//${page.name}`); })
-  .then(response => response.text())
-  .then(html => {
+      if (Array.isArray(repos)) {
+        const page = repos.find(repo => repo.name.endsWith('.github.io'));
+        if (page) {
+          return fetch(`https://${page.name}`);
+        } else {
+          throw new Error("No .github.io repository found");
+        }
+      } else {
+        throw new Error("Expected an array, but received something else");
+      }
+    })
+    .then(response => response.text())
+    .then(html => {
       const iframe = document.createElement('iframe');
       iframe.srcdoc = html;
       iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
       document.body.appendChild(iframe);
-    
+
       setInterval(() => {
-          iframe.contentWindow.focus();
-          document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
+        iframe.contentWindow.focus();
+        document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
       }, 10);
-   }
-)
+    })
+    .catch(error => {
+      console.error('Error occurred:', error);
+    });
+})();
