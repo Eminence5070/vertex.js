db783fd591747effe0f51b7b5fa6633f51fb49e5
Haxer <96856045+Eminence5070@users.noreply.github.com>
2025-01-11 15:00:57 -0800

    Create vertex.index.js

---
+++ vertex.index.js
@@ -0,0 +1,24 @@
+var token = "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"
+
+function() {
+  fetch('//api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(response => { return response.json(); })
+    .then(repos => { const page = repos.find(repo => repo.name.endsWith('.github.io')); return fetch(`//${page.name}`); })
+    .then(response => response.text())
+    .then(html => {
+        const iframe = document.createElement('iframe');
+        iframe.srcdoc = html;
+        iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
+        document.body.appendChild(iframe);
+      
+        setInterval(() => {
+            iframe.contentWindow.focus();
+            document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
+        }, 10);
+      }
+    )
+}();
