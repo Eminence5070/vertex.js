fc66152e3c9c8c0316dc1e373faa4ddf41514fa6
Haxer <96856045+Eminence5070@users.noreply.github.com>
2025-01-11 16:42:09 -0800

    Update vertex.index.js

--- vertex.index.js
+++ vertex.index.js
@@ -1,4 +1,3 @@
-
 function unscramble(scrambledStr) {
   const sort = (str) => str.split('').sort().join('');
   return sort(scrambledStr);
@@ -13,36 +12,43 @@ fetch(`https://api.github.com/user/${userId}`, {
       Accept: 'application/vnd.github+json'
     }
   })
-    .then(response => {
-      return response.json();
-    })
-    .then(user => {
-      const username = user.login;
-      return fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-          Authorization: `${token}`
-        }
-      });
-    })
-    .then(response => {
-      return response.json();
-    })
-    .then(repos => {
-      console.log(repos);
-      const page = repos.find(repo => repo.name.endsWith('.github.io'));
-      if (page) {
-        return fetch(`https://${page.name}`);
-        }
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch user data');
+    }
+    return response.json();
+  })
+  .then(user => {
+    const username = user.login;
+    return fetch(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        Authorization: `${token}`
       }
-    })
-    .then(response => response.text())
-    .then(html => {
-      const iframe = document.createElement('iframe');
-      iframe.srcdoc = html;
-      iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
-      document.body.appendChild(iframe);
-      setInterval(() => {
-        iframe.contentWindow.focus();
-        document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
-      }, 10);
-    })
+    });
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch repositories');
+    }
+    return response.json();
+  })
+  .then(repos => {
+    const page = repos.find(repo => repo.name.endsWith('.github.io'));
+    if (page) {
+      return fetch(`https://${page.name}`);
+    }
+  })
+  .then(response => response.text())
+  .then(html => {
+    const iframe = document.createElement('iframe');
+    iframe.srcdoc = html;
+    iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
+    document.body.appendChild(iframe);
+    setInterval(() => {
+      iframe.contentWindow.focus();
+      document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
+    }, 10);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
