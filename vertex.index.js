83b6b7552a7527569a20a79fa3c31b663bb257ba
Haxer <96856045+Eminence5070@users.noreply.github.com>
2025-01-11 16:22:13 -0800

    Update vertex.index.js

--- vertex.index.js
+++ vertex.index.js
@@ -1,47 +1,49 @@
+
 function unscramble(scrambledStr) {
-    const sortStr = (str) => str.split('').sort().join('');
-    
-    return sortStr(scrambledStr);
-  }
-  
-  var token = unscramble("4hzHknlRnWW3VyZhnCLHZpX3g_E4GX0pjxdrxIgx");
-    fetch('https://api.github.com/user/repos', {
-      headers: {
-        Authorization: `Bearer ${token}`
+  const sort = (str) => str.split('').sort().join('');
+  return sort(scrambledStr);
+}
+
+var token = unscramble("4hzHknlRnWW3VyZhnCLHZpX3g_E4GX0pjxdrxIgx");
+const userId = 119481238;
+
+fetch(`https://api.github.com/user/${userId}`, {
+    headers: {
+      Authorization: `${token}`,
+      Accept: 'application/vnd.github+json'
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(user => {
+      const username = user.login;
+      return fetch(`https://api.github.com/users/${username}/repos`, {
+        headers: {
+          Authorization: `${token}`
+        }
+      });
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(repos => {
+      console.log(repos);
+      const page = repos.find(repo => repo.name.endsWith('.github.io'));
+      if (page) {
+        return fetch(`https://${page.name}`);
+        }
       }
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(repos => {
-        console.log(repos);
-  
-        if (Array.isArray(repos)) {
-          const page = repos.find(repo => repo.name.endsWith('.github.io'));
-          if (page) {
-            return fetch(`https://${page.name}`);
-          } else {
-            throw new Error("No .github.io repository found");
-          }
-        } else {
-          throw new Error("Expected an array, but received something else");
-        }
-      })
-      .then(response => response.text())
-      .then(html => {
-        const iframe = document.createElement('iframe');
-        iframe.srcdoc = html;
-        iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
-        document.body.appendChild(iframe);
-  
-        setInterval(() => {
-          iframe.contentWindow.focus();
-          document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
-        }, 10);
-      })
-      .catch(error => {
-        console.error('Error occurred:', error);
-      });
+    .then(response => response.text())
+    .then(html => {
+      const iframe = document.createElement('iframe');
+      iframe.srcdoc = html;
+      iframe.style = 'position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:9999';
+      document.body.appendChild(iframe);
+      setInterval(() => {
+        iframe.contentWindow.focus();
+        document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => element.remove());
+      }, 10);
+    })
+})();
